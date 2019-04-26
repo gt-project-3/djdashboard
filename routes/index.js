59e1108d71882a8796ae14a081ac7a8447d922d7
Bente Gber <benjamingreenberg@Benjamins-MacBook-Pro-2.local>
2019-04-26 19:10:00 -0400

    added more routes and controllers

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,26 @@ router
 router
   .route('/event')
   .get(controllers.Event.findAll)
-  .post(controllers.Event.create)
-  .delete(controllers.Event.remove);
+  .post(controllers.Event.create);
 router
-  .route('/user')
-  .get(controllers.User.findById)
+.route('/user')
+.post(controllers.User.create);
+
+router
+.route('/user/:id')
+.get(controllers.User.findById)
+.delete(controllers.User.remove)
+.put(controllers.User.update);
+
+router
+  .route('/songs')
+  .get(controllers.Dj.findById)
   .post(controllers.User.create)
   .delete(controllers.User.remove);
-
+router
+  .route('/event/:id')
+  .get(controllers.Event.findById)
+  .post(controllers.Event.create)
+  .put(controllers.Event.update)
+  .delete(controllers.Event.remove);
 module.exports = router;
